38d66e0b4d41c21f2496ca3a44eb7b6033b68e1e
Sjors Wijsman <sjorswijsman@gmail.com>
2020-10-22 16:18:45 +0200

    [Added] Oefening data opschonen - oogkleur

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const splitTekens = [",", ".", ";", "&"];
 keukenData = keukenData.map(entry => splitString(entry, splitTekens));
 
 // Split string op meerdere delimiters
-function splitString(string, delimiters) {
+function splitString(string, delimiters = [",", ".", ";"]) {
   for (delimiter of delimiters) {
     // Split op delimiter en rejoin naar string
     string = string.split(delimiter).join(",");
@@ -75,11 +75,11 @@ keukenData = keukenData.map(entry => maakValideKeukenNaam(entry));
 
 // Check & valideer keuken type
 function maakValideKeukenNaam(entry) {
-  let checkIfReturned = false;
   let newEntry = []
 
   // Loop door alle keukens heen in de entry
   for (keuken of entry) {
+    let checkIfReturned = false;
     const eersteVierLetters = keuken.substring(0, 4).toLowerCase();
     if (keukenList[eersteVierLetters]) {
       newEntry.push(keukenList[eersteVierLetters]);
@@ -93,6 +93,71 @@ function maakValideKeukenNaam(entry) {
   return newEntry
 }
 
+
+
+/*
+    oogKleur
+*/
+// Haal oogkleur kolom uit data
+let oogKleurData = data.map(entry => entry.oogKleur);
+
+// Data naar uppercase
+oogKleurData = oogKleurData.map(entry => entry.toUpperCase());
+
+
+// Lijst van alle oogkleuren
+oogKleurLijst = {
+  "BLAUW": "#7BB3FA",
+  "LICHTBLAUW": "#87CEFA",
+  "BRUIN": "#693704",
+  "GROEN": "#76CE09",
+}
+
+const validatedOogKleurData = oogKleurData.map(entry => valideerOogKleur(entry));
+
+// Valideer oogkleur op hex, textueel en rgb
+function valideerOogKleur(oogKleur) {
+  // Verwijder spaties
+  oogKleur = oogKleur.replace(/\s/g, "");
+
+  // Verwijder hekje
+  oogKleur = oogKleur.replace("#", "");
+
+  // Valideer hex
+  // https://mkyong.com/regular-expressions/how-to-validate-hex-color-code-with-regular-expression/
+  if (/^([A-F0-9]{6}|[A-F0-9]{3})$/.test(oogKleur)) {
+    return "#" + oogKleur;
+  }
+
+  // Valideer textuele kleur
+  else if (oogKleurLijst[oogKleur]) {
+    return oogKleurLijst[oogKleur];
+  }
+
+  // Valideer RGB
+  else if (oogKleur.includes("RGB")) {
+    oogKleurRGB = oogKleur.replace("RGB(", "").replace(")", "");
+    let hexCode = "#";
+    // Loop door RGB values heen
+    for (kleur of splitString(oogKleurRGB)) {
+      // Check of kleur binnen max rgb waarde valt
+      if (kleur >= 0 && kleur <= 255) {
+        // Convert kleur nummer naar hexwaarde
+        hexCode += parseInt(kleur).toString(16);
+      } else {
+        return "invalide RGB: " + oogKleur;
+      }
+    }
+    return hexCode;
+  }
+
+  else {
+    return "idk wat deze kleur is: " + oogKleur;
+  }
+}
+
+console.log(oogKleurData);
+console.log(validatedOogKleurData);
 
 
 /*
